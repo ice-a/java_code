2576a6b4d01fb16a7bb651f00a172cda69e7aee8
爱喝水 <1943158197@qq.com>
2023-01-08 23:00:41 +0800

    add info

---
+++ src/Demo.java
@@ -0,0 +1,13 @@
+// learn_1 https://www.bilibili.com/video/BV1MG4y1r77s?p=87&spm_id_from=pageDriver&vd_source=6d4cf94814ff58c7897075ba9698f191
+// learn_2 https://www.bilibili.com/video/BV1MG4y1r77s?p=98&vd_source=6d4cf94814ff58c7897075ba9698f191
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello info");
+    }
+}
+
+class demo2 {
+    public static void main(String[] args) {
+        System.out.print("code info");
+    }
+}
---
+++ src/info/TestA.java
@@ -0,0 +1,9 @@
+package info;
+
+public class TestA {
+    public static void main(String[] args) {
+        int a = 123;
+        String b = "字符串";
+        System.out.println(a + b);
+    }
+}
